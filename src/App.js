6587e6b1f5a5a38c04153859a0ba2735351f62f3
dsa-2002 <133044607+dsa-2002@users.noreply.github.com>
2023-08-24 01:47:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,97 +1,4 @@
-// import React from "react";
-// import { nanoid } from "nanoid";
-// import NotesList from "./components/NotesList";
-// import Search from "./components/Search";
-// import Header from "./components/Header";
-
-
-
-// export default function App() {
-
-//   const [notes, setNotes] = React.useState([
-//     {
-//       id: nanoid(),
-//       text: "This is my first note!",
-//       date: "10/04/2023",
-//     },
-//     {
-//       id: nanoid(),
-//       text: "This is my second note!",
-//       date: "10/04/2023",
-//     },
-//     {
-//       id: nanoid(),
-//       text: "This is my third note!",
-//       date: "10/04/2023",
-//     },
-//     {
-//       id: nanoid(),
-//       text: "This is my new note!",
-//       date: "10/04/2023",
-//     },
-//   ]);
-
-
-
-//   const [searchText, setSearchText] = React.useState("");
-
-//   const [darkMode, setDarkMode] = React.useState(false);
-
-
-//   React.useEffect(() => {
-//     const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
-
-//     if (savedNotes) {
-//       setNotes(savedNotes);
-//     }
-//   }, []);
-
-
-
-//   React.useEffect(() => {
-//     localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
-//   }, [notes]);
-
-
-
-//   const addNote = (text) => {
-//     const date = new Date();
-//     const newNote = {
-//       id: nanoid(),
-//       text: text,
-//       date: date.toLocaleDateString(),
-//     };
-//     const newNotes = [...notes, newNote];
-//     setNotes(newNotes);
-//   };
-
-
-
-//   const deleteNote = (id) => {
-//     const newNotes = notes.filter((note) => note.id !== id);
-//     setNotes(newNotes);
-//   };
-
-
-
-//   return (
-//     <div className={`${darkMode && "dark-mode"}`}>
-//       <div className="container">
-//         <Header handleToggleDarkMode={setDarkMode} />
-//         <Search handleSearchNote={setSearchText} />
-//         <NotesList
-//           notes={notes.filter((note) =>
-//             note.text.toLowerCase().includes(searchText)
-//           )}
-//           handleAddNote={addNote}
-//           handleDeleteNote={deleteNote}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-
+import React from "react"
 import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
@@ -103,22 +10,22 @@ const App = () => {
 		{
 			id: nanoid(),
 			text: 'This is my first note!',
-			date: '15/04/2021',
+			date: '15/04/2023',
 		},
 		{
 			id: nanoid(),
 			text: 'This is my second note!',
-			date: '21/04/2021',
+			date: '21/01/2022',
 		},
 		{
 			id: nanoid(),
 			text: 'This is my third note!',
-			date: '28/04/2021',
+			date: '28/03/2021',
 		},
 		{
 			id: nanoid(),
 			text: 'This is my new note!',
-			date: '30/04/2021',
+			date: '30/02/2023',
 		},
 	]);
 
